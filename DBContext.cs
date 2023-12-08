731d6799d9fbd3ce70c524cb19119921b5f5aa5f
Osttik <lololka23@gmail.com>
2023-12-08 05:55:55 +0200

    Initial

---
+++ DBContext.cs
@@ -0,0 +1,48 @@
+﻿using CryptoServices.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoServices.Data
+{
+    public partial class DBContext : DbContext
+    {
+        public DBContext()
+        {
+        }
+
+        public DBContext(DbContextOptions<DBContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedOnAdd();
+            });
+
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedOnAdd();
+
+                entity.HasOne(e => e.User)
+                .WithMany(e => e.Orders)
+                .HasForeignKey(e => e.UserId)
+                .IsRequired();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static MassTransit.MessageHeaders;
+
+namespace CryptoServices.Data.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Order> Orders { get; }
+    }
+}

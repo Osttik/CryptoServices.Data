f27cc63c7bddda285aac19ab32caeab069cdd246
Osttik <lololka23@gmail.com>
2023-12-11 05:56:45 +0200

    Add Current state for order

---
+++ Migrations/20231211035623_update order with state.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CryptoServices.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateorderwithstate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CurrentState",
+                table: "Orders",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrentState",
+                table: "Orders");
+        }
+    }
+}
--- Models/Order.cs
+++ Models/Order.cs
@@ -9,6 +9,7 @@ namespace CryptoServices.Data.Models
     public class Order
     {
         public Guid Id { get; set; }
+        public string CurrentState { get; set; }
         public Guid UserId { get; set; }
         public User User { get; set; }
     }
